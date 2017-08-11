d77467bb3aef630bdd6864c84f48c48e6c63cb6b
firelk <58009015@qq.com>
2017-08-11 11:58:58 +0800

    v1.0.11

--- js/loan/uploadimage2.js
+++ js/loan/uploadimage2.js
@@ -248,7 +248,7 @@ function postImgIphone(base64, imgId, inputId) {
             }
         }
     }
-    windiow.timeoutFn = function timeoutFn(imgId){
+    window.timeoutFn = function timeoutFn(imgId){
         $.alert('上传失败，请重试!');
         console.log(imgId)
         var pic = $('#' + imgId);
